dc4d6c124935fd8d144a6da8a403a36ff1dc291a
Romina Vega y Verdugo <romiivega3005@gmail.com>
2019-08-13 07:50:27 -0700

    vf tgdc5eu

---
+++ ModificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cuenta
+    {
+
+        public Cliente Propietario { get; set ;} 
+        public float saldo { get; set ;}
+        private string _identificador;
+        public string Identificador
+        {
+            get
+            {
+                return _identificador;
+            }
+            set
+            {
+                if (value.Length < 10 && value.Length  <=14  )
+                {
+                    _identificador = value;
+                }
+                else
+                {
+                    throw new Exception("No es entre 10 y 14 caracteres");
+                }
+            }
+        }
+
+
+
+
+    }
+}
--- ModificadoresDeAcceso/Program.cs
+++ ModificadoresDeAcceso/Program.cs
@@ -27,6 +27,24 @@ namespace ModificadoresDeAcceso
             {
                 Console.WriteLine ("La curp no es de 18 caracteres");
             }
+            Cuenta cuenta
+                = new Cuenta();
+            cuenta.Propietario = "";
+            cuenta.saldo = 864502864576;
+
+
+            Console.WriteLine(" Propietario : " + cuenta.Propietario);
+            Console.WriteLine("Saldo : " + cuenta.saldo);
+            try
+            {
+                cuenta.Identificador = " 123456789012334";
+                Console.WriteLine(" Indetificador: " + cuenta.Identificador);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" El identificador no incluye de 10 a 14 caracteres");
+            }
+
             Console.Read();
         }
     }
